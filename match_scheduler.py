c512fbf8e3015ed2d81f5a6b5ce0094867c1adf1
Joseph Scollo <joseph.scollo@payroc.com>
2025-03-14 10:54:54 -0400

    added two test calls

--- match_scheduler.py
+++ match_scheduler.py
@@ -1,10 +1,8 @@
 import sys
 import unittest
-
 import Testing.unit_tests
 from Testing import unit_tests
 from Testing.unit_tests import LeagueTests
-
 
 def run_unit_tests():
 
@@ -13,6 +11,9 @@ def run_unit_tests():
     tests.all_leagues_have_unique_teams()
     tests.all_leagues_have_unique_locations()
     tests.all_teams_have_a_location()
+    tests.all_locations_have_at_least_one_team()
+    tests.number_of_matches_is_number_of_teams_multiplied_by_number_of_teams_minus_one()
+
     print("Unit tests passed")
 
 def main(debug):
